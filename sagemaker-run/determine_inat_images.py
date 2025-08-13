5506d1035d0abf76c84ba46764b24e964e2af29f
Erem Kazancioglu <erem.kazancioglu@gmail.com>
2025-08-13 14:55:48 -0400

    Changed script that determines which images we need to work from CLI

--- sagemaker-run/determine_inat_images.py
+++ sagemaker-run/determine_inat_images.py
@@ -1,94 +1,104 @@
 import pandas as pd
 
-balto_min_lat = 39.19
-balto_max_lat = 39.37
-balto_min_lon = -1.*76.71
-balto_max_lon = -1.*76.52
+def main():
 
-maryland_min_lat = 37.911717
-maryland_max_lat = 39.723043
-maryland_min_lon = -1.*79.487651
-maryland_max_lon = -1.*75.048939
+	# Baltimore lat lon
+	balto_min_lat = 39.19
+	balto_max_lat = 39.37
+	balto_min_lon = -1.*76.71
+	balto_max_lon = -1.*76.52
 
-target_species = ['Lycorma delicatula', 'Zenaida macroura', 'Acer rubrum', 'Erigeron canadensis']
+	# Maryland lat lon
+	maryland_min_lat = 37.911717
+	maryland_max_lat = 39.723043
+	maryland_min_lon = -1.*79.487651
+	maryland_max_lon = -1.*75.048939
 
-acer_like_species = ["Acer saccharum", "Acer saccharinum", "Acer platanoides"]
+	target_species = ['Lycorma delicatula', 'Zenaida macroura', 'Acer rubrum', 'Erigeron canadensis'] #Update this if you need other species
 
-species_to_get_from_md = target_species + acer_like_species
+	acer_like_species = ["Acer saccharum", "Acer saccharinum", "Acer platanoides"] #This can be empty if you don't have the problem like Acer does
 
-chunk_size = 10000
-maryland_observations = []
-min_lat = 37.911717
-max_lat = 39.723043
-min_lon = -1.*79.487651
-max_lon = -1.*75.048939
-for chunk_number, chunk in enumerate(pd.read_csv('./observations.csv', chunksize=chunk_size, sep = '\t')):
+	species_to_get_from_md = target_species + acer_like_species
 
-    filtered_chunk = chunk.query(f"(latitude <= {maryland_max_lat}) & (latitude >= {maryland_min_lat}) & (longitude <= {maryland_max_lon}) & (longitude >= {maryland_min_lon})")
+	# Determine which observations are from Maryland
+	chunk_size = 10000
+	maryland_observations = []
+	for chunk_number, chunk in enumerate(pd.read_csv('./observations.csv', chunksize=chunk_size, sep = '\t')):
 
-    if not filtered_chunk.empty:
-        print(f"Appending {len(filtered_chunk)} records")
-        maryland_observations.append(filtered_chunk)
-        
-maryland_obs_df = pd.concat(maryland_observations, ignore_index=True)
+	    filtered_chunk = chunk.query(f"(latitude <= {maryland_max_lat}) & (latitude >= {maryland_min_lat}) & (longitude <= {maryland_max_lon}) & (longitude >= {maryland_min_lon})")
 
-taxa_df = pd.read_csv('./taxa.csv', index_col=None, sep='\t')
+	    if not filtered_chunk.empty:
+	        print(f"Appending {len(filtered_chunk)} records")
+	        maryland_observations.append(filtered_chunk)
+	maryland_obs_df = pd.concat(maryland_observations, ignore_index=True)
 
-maryland_obs_df = pd.merge(maryland_obs_df, taxa_df[["taxon_id", "name"]], on = "taxon_id")
+	taxa_df = pd.read_csv('./taxa.csv', index_col=None, sep='\t')
+	maryland_obs_df = pd.merge(maryland_obs_df, taxa_df[["taxon_id", "name"]], on = "taxon_id")
 
-balto_obs_df = maryland_obs_df.query(f"(latitude <= {balto_max_lat}) & (latitude >= {balto_min_lat}) & (longitude <= {balto_max_lon}) & (longitude >= {balto_min_lon})")
 
-maryland_obs_uuids = maryland_obs_df[maryland_obs_df.name.isin(species_to_get_from_md)]["observation_uuid"].unique()
+	# Determine which observations are from Baltimore
+	balto_obs_df = maryland_obs_df.query(f"(latitude <= {balto_max_lat}) & (latitude >= {balto_min_lat}) & (longitude <= {balto_max_lon}) & (longitude >= {balto_min_lon})")
 
-chunk_size = 10000
-maryland_uuid_photo_id_ext = []
-for chunk_number, chunk in enumerate(pd.read_csv('./photos.csv', chunksize=chunk_size, sep = '\t')):
+	# Relevant observations from Maryland
+	maryland_obs_uuids = maryland_obs_df[maryland_obs_df.name.isin(species_to_get_from_md)]["observation_uuid"].unique()
 
-    filtered_chunk = chunk[chunk["observation_uuid"].isin(maryland_obs_uuids)]
+	# Determine which images we need from all of Maryland
+	chunk_size = 10000
+	maryland_uuid_photo_id_ext = []
+	for chunk_number, chunk in enumerate(pd.read_csv('./photos.csv', chunksize=chunk_size, sep = '\t')):
 
-    if not filtered_chunk.empty:
-        unique_uuid_photo_id_ext = filtered_chunk[["observation_uuid", 'photo_id', 'extension']].drop_duplicates()
-        uuid_photo_ids_ext = list(zip(unique_uuid_photo_id_ext['observation_uuid'], unique_uuid_photo_id_ext['photo_id'], unique_uuid_photo_id_ext['extension']))
+	    filtered_chunk = chunk[chunk["observation_uuid"].isin(maryland_obs_uuids)]
 
-        print(f"Appending {len(uuid_photo_ids_ext)} records")
-        
-        maryland_uuid_photo_id_ext += uuid_photo_ids_ext
+	    if not filtered_chunk.empty:
+	        unique_uuid_photo_id_ext = filtered_chunk[["observation_uuid", 'photo_id', 'extension']].drop_duplicates()
+	        uuid_photo_ids_ext = list(zip(unique_uuid_photo_id_ext['observation_uuid'], unique_uuid_photo_id_ext['photo_id'], unique_uuid_photo_id_ext['extension']))
 
-maryland_obs_with_photos =\
-pd.DataFrame(maryland_uuid_photo_id_ext,\
-             columns = ["observation_uuid", "photo_id", "extension"]).merge(maryland_obs_df[['observation_uuid', 'name']], on = 'observation_uuid')
+	        print(f"Appending {len(uuid_photo_ids_ext)} records")
+	        
+	        maryland_uuid_photo_id_ext += uuid_photo_ids_ext
 
-maryland_obs_with_photos["image_name"] = maryland_obs_with_photos.apply(lambda x: str(x["photo_id"]) + "_medium." + x["extension"], axis = 1)
+	maryland_obs_with_photos =\
+	pd.DataFrame(maryland_uuid_photo_id_ext,\
+	             columns = ["observation_uuid", "photo_id", "extension"]).merge(maryland_obs_df[['observation_uuid', 'name']], on = 'observation_uuid')
 
-balto_obs_uuids = balto_obs_df[~(balto_obs_df.name.isin(species_to_get_from_md))]["observation_uuid"].unique()
+	maryland_obs_with_photos["image_name"] = maryland_obs_with_photos.apply(lambda x: str(x["photo_id"]) + "_medium." + x["extension"], axis = 1)
 
-chunk_size = 10000
-balto_uuid_photo_id_ext = []
-for chunk_number, chunk in enumerate(pd.read_csv('./photos.csv', chunksize=chunk_size, sep = '\t')):
+	# Relevant observations from Baltimore
+	balto_obs_uuids = balto_obs_df[~(balto_obs_df.name.isin(species_to_get_from_md))]["observation_uuid"].unique()
 
-    filtered_chunk = chunk[chunk["observation_uuid"].isin(balto_obs_uuids)]
+	# Determine which images we need from Baltimore
+	chunk_size = 10000
+	balto_uuid_photo_id_ext = []
+	for chunk_number, chunk in enumerate(pd.read_csv('./photos.csv', chunksize=chunk_size, sep = '\t')):
 
-    if not filtered_chunk.empty:
-        unique_uuid_photo_id_ext = filtered_chunk[["observation_uuid", 'photo_id', 'extension']].drop_duplicates()
-        uuid_photo_ids_ext = list(zip(unique_uuid_photo_id_ext['observation_uuid'], unique_uuid_photo_id_ext['photo_id'], unique_uuid_photo_id_ext['extension']))
+	    filtered_chunk = chunk[chunk["observation_uuid"].isin(balto_obs_uuids)]
 
-        print(f"Appending {len(uuid_photo_ids_ext)} records")
-        
-        balto_uuid_photo_id_ext += uuid_photo_ids_ext
+	    if not filtered_chunk.empty:
+	        unique_uuid_photo_id_ext = filtered_chunk[["observation_uuid", 'photo_id', 'extension']].drop_duplicates()
+	        uuid_photo_ids_ext = list(zip(unique_uuid_photo_id_ext['observation_uuid'], unique_uuid_photo_id_ext['photo_id'], unique_uuid_photo_id_ext['extension']))
 
-balto_obs_with_photos =\
-pd.DataFrame(balto_uuid_photo_id_ext,\
-             columns = ["observation_uuid", "photo_id", "extension"]).merge(balto_obs_df[['observation_uuid', 'name']], on = 'observation_uuid')
+	        print(f"Appending {len(uuid_photo_ids_ext)} records")
+	        
+	        balto_uuid_photo_id_ext += uuid_photo_ids_ext
 
-balto_obs_with_photos["image_name"] = balto_obs_with_photos.apply(lambda x: str(x["photo_id"]) + "_medium." + x["extension"], axis = 1)
+	balto_obs_with_photos =\
+	pd.DataFrame(balto_uuid_photo_id_ext,\
+	             columns = ["observation_uuid", "photo_id", "extension"]).merge(balto_obs_df[['observation_uuid', 'name']], on = 'observation_uuid')
 
-combo_subsample = pd.concat([balto_obs_with_photos.sample(frac=0.1).rename(columns =\
-                                                                           {"name": "species_name"})[['species_name', 'image_name', 'photo_id', 'extension']].drop_duplicates(),\
-                             maryland_obs_with_photos.rename(columns =\
-                                                             {"name": "species_name"})[['species_name', 'image_name', 'photo_id', 'extension']].drop_duplicates()], ignore_index=True)
+	balto_obs_with_photos["image_name"] = balto_obs_with_photos.apply(lambda x: str(x["photo_id"]) + "_medium." + x["extension"], axis = 1)
 
-combo_subsample[combo_subsample.duplicated(keep=False)].drop_duplicates()
 
-combo_subsample = combo_subsample[~combo_subsample.duplicated(subset=['image_name', 'photo_id', 'extension'], keep=False)]
+	# Combine Maryland observations with Baltimore observations, downsampling the latter to 10%
+	combo_subsample = pd.concat([balto_obs_with_photos.sample(frac=0.1).rename(columns =\
+	                                                                           {"name": "species_name"})[['species_name', 'image_name', 'photo_id', 'extension']].drop_duplicates(),\
+	                             maryland_obs_with_photos.rename(columns =\
+	                                                             {"name": "species_name"})[['species_name', 'image_name', 'photo_id', 'extension']].drop_duplicates()], ignore_index=True)
 
-combo_subsample.to_csv("./training_images.csv", index=None)
+	combo_subsample[combo_subsample.duplicated(keep=False)].drop_duplicates()
+
+	combo_subsample = combo_subsample[~combo_subsample.duplicated(subset=['image_name', 'photo_id', 'extension'], keep=False)]
+
+	combo_subsample.to_csv("./training_images.csv", index=None)
+
+if __name__ == "__main__":
+	main()
